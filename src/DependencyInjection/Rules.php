1dcc46fe06ff4fc25d2fd3bdc678abd8c4e70598
Bruno Conte <brunoconte3@gmail.com>
2022-08-08 15:58:40 -0300

    retirado item depreciado, setar false em contexto nao boleano

--- src/DependencyInjection/Rules.php
+++ src/DependencyInjection/Rules.php
@@ -13,7 +13,7 @@ class Rules
     use TraitRuleFile;
     use TraitRuleString;
 
-    protected mixed $errors = false;
+    protected mixed $errors;
     public const RULES_WITHOUT_FUNCS = ['convert'];
 
     private function invalidRule(
